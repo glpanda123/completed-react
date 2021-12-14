6935cfa1ade2d1b3478ecd9c8fab06f8850fcb3b
Hoang Nguyen <glpanda123@gmail.com>
2021-12-14 08:22:51 +0700

    dump and smart component

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,15 +1,23 @@
+import { useState } from 'react/cjs/react.development';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 function Expenses(props) {
+  const [year, setYear] = useState('2021');
+
+  const selectYearHandler = (selectedYear) => setYear(selectedYear);
   return (
-    <Card className='expenses'>
-      <ExpenseItem className='App-link' expenseItem={props.expenses[0]}></ExpenseItem>
-      <ExpenseItem className='App-link' expenseItem={props.expenses[1]}></ExpenseItem>
-      <ExpenseItem className='App-link' expenseItem={props.expenses[2]}></ExpenseItem>
-      <ExpenseItem className='App-link' expenseItem={props.expenses[3]}></ExpenseItem>
-    </Card>
+    <div>
+      <ExpenseFilter onSelectYear={selectYearHandler} year={year}></ExpenseFilter>
+      <Card className='expenses'>
+        <ExpenseItem className='App-link' expenseItem={props.expenses[0]}></ExpenseItem>
+        <ExpenseItem className='App-link' expenseItem={props.expenses[1]}></ExpenseItem>
+        <ExpenseItem className='App-link' expenseItem={props.expenses[2]}></ExpenseItem>
+        <ExpenseItem className='App-link' expenseItem={props.expenses[3]}></ExpenseItem>
+      </Card>
+    </div>
   );
 }
 
